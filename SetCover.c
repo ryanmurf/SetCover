cb6ff6df929d3d9e24d00db45bd921a0c360c4cf
Ryan Murphy <ryanmurf@gmail.com>
2014-12-01 16:59:53 -0700

    Added code to find the greedy solution and use that for starting point of the backtracking.

--- SetCover.c
+++ SetCover.c
@@ -56,6 +56,8 @@ void backTrack4(Solution* solution, int last, int sum);
 void createSolutionStruct();
 void copySolutionToBest();
 void sortSubSets();
+void greedy();
+int numberOfUncoveredElements(int subSetIndex);
 
 void intHandler(int dummy) {
 	printSolution(bestSolution);
@@ -70,8 +72,9 @@ int main(int argc, char *argv[]) {
 	readGameFile(_file);
 	sortSubSets();
 	//echoInit();
-
 	createSolutionStruct();
+
+	greedy();
 
 	backTrack4(solution, 0, 0);
 
@@ -79,6 +82,43 @@ int main(int argc, char *argv[]) {
 	printf("\n");
 
 	return 0;
+}
+
+void greedy() {
+	int i;
+	int temp;
+	bestSolution->nSolutionSize = 0;
+	addSubSet(bestSolution, 0);
+	int addIndex = 0;
+	int addNumber = 0;
+
+	while(!checkSolution(bestSolution)) {
+		addIndex = 0;
+		addNumber = 0;
+		for(i=0; i<set->nSubSets; i++) {
+			if(!containsSubSet(bestSolution, i)) {
+				temp = numberOfUncoveredElements(i);
+				if(temp > addNumber) {
+					addNumber = temp;
+					addIndex = i;
+				}
+			}
+		}
+		addSubSet(bestSolution, addIndex);
+	}
+	//printSolution(bestSolution);
+}
+
+int numberOfUncoveredElements(int subSetIndex) {
+	int i;
+	int count=0;
+
+	for(i=0; i<set->nSubSetSizes[subSetIndex]; i++) {
+		if(!bestSolution->boolIncluded[set->subsets[subSetIndex][i] - 1])
+			count++;
+	}
+
+	return count;
 }
 
 //This sorts the subsets by size
@@ -140,7 +180,7 @@ void createSolutionStruct() {
 		bestSolution->subSets[i] = -1;
 	bestSolution->boolIncluded = malloc(sizeof(int) * set->nGlobalSetSize);
 	memset(bestSolution->boolIncluded, 0, set->nGlobalSetSize);
-	bestSolution->nSolutionSize = (int)set->nSubSets-1;
+	bestSolution->nSolutionSize = set->nSubSets-1;
 }
 
 void copySolutionToBest() {
